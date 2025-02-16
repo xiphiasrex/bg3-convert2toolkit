c0932a2602114fb217180eba71e683ac7c3d3447
Eclip5e <32602757+Eclip5eLP@users.noreply.github.com>
2025-02-16 12:25:57 +0100

    Fixed crash on non locale files

--- fixlocale.py
+++ fixlocale.py
@@ -7,30 +7,33 @@ import os
 
 class FixLocale():
 	def fix(self, file, conv):
-		data = conv.readxml(file)
-		dupes = 0
-		vfix = 0
-		db = {}
-		construct = []
+		try:
+			data = conv.readxml(file)
+			dupes = 0
+			vfix = 0
+			db = {}
+			construct = []
 
-		# Fix dupes
-		for x in data["contentList"]["content"]:
-			if not x['@contentuid'] in db.keys():
-				construct.append(x)
-				db[x['@contentuid']] = x['@version']
-				continue
-			dupes += 1
-			#print(x['@contentuid'])
-			if int(x['@version']) > int(db[x['@contentuid']]):
-				db[x['@contentuid']] = x['@version']
+			# Fix dupes
+			for x in data["contentList"]["content"]:
+				if not x['@contentuid'] in db.keys():
+					construct.append(x)
+					db[x['@contentuid']] = x['@version']
+					continue
+				dupes += 1
+				#print(x['@contentuid'])
+				if int(x['@version']) > int(db[x['@contentuid']]):
+					db[x['@contentuid']] = x['@version']
 
-		# Set Versions all to 1
-		for x in construct:
-			if x['@version'] != '1':
-				vfix += 1
-			x['@version'] = '1'
+			# Set Versions all to 1
+			for x in construct:
+				if x['@version'] != '1':
+					vfix += 1
+				x['@version'] = '1'
 
-		data["contentList"]["content"] = construct
-		conv.writexml(data, str(file).replace('.xml', '_fix.xml'))
-		print(f'{Fore.GREEN}[locale] Fixed {os.path.basename(file)} (Duplicates: {dupes}; Version Resets: {vfix}){Fore.WHITE}')
-		return True
+			data["contentList"]["content"] = construct
+			conv.writexml(data, str(file).replace('.xml', '_fix.xml'))
+			print(f'{Fore.GREEN}[locale] Fixed {os.path.basename(file)} (Duplicates: {dupes}; Version Resets: {vfix}){Fore.WHITE}')
+			return True
+		except Exception as e:
+			return False
